3ff9bfcb0248c3dda90d9b4f6b6359c66d5b1f1a
Rev1932 <victorburkert2019@gmail.com>
2025-10-29 22:38:40 -0300

    logica de metodos gets feita

--- get_factory.py
+++ get_factory.py
@@ -14,7 +14,7 @@ class GetDistribution():
             "corredor": GetCorredor,
             "empresas": GetEmpresas,
             "previsao": GetPrevisao,
-            "previsao_linha": GetPrevisoLinha,
+            "previsao_linha": GetPrevisaoLinha,
             "previsao_parada": GetPrevisaoParada,
         }
 
@@ -65,6 +65,114 @@ class GetLinhaBuscar() :
         self.api_token = api_token
         self.base_url = base_url
         self.get = f"{args.get}/Buscar?termosBusca={args.linha}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetLinhaBuscarSentido() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/BuscarLinhaSentido?termosBusca={args.linha}&sentido={args.sentido}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetParadasBuscar() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/Buscar?termosBusca={args.parada}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetParadasBuscarLinha() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/BuscarParadasPorLinha?codigoLinha={args.linha}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetParadasBuscarCorredor() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/BuscarParadasPorCorredor?codigoCorredor={args.corredor}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetCorredor() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetEmpresas() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetPrevisao() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/?codigoParada={args.parada}&codigoLinha={args.linha} " 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetPrevisaoLinha() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/Linha?codigoLinha={args.linha}" 
+
+    def run(self):
+        connect = ApiConection(self.api_token, self.base_url, self.get)
+        session = connect.auth()
+        data = connect.get_data(session)
+        connect.save_data(data, 'dados')
+
+class GetPrevisaoParada() :
+    def __init__(self, api_token, base_url, args):
+        self.api_token = api_token
+        self.base_url = base_url
+        self.get = f"{args.get}/Parada?codigoParada={args.parada}" 
 
     def run(self):
         connect = ApiConection(self.api_token, self.base_url, self.get)
